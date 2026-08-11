15eeb9db8ffcfccc58140abedf9bd3c77bfb416b
julimuli1997 <julian-oehmichen@web.de>
2026-08-11 12:58:54 +0200

    Another change in Header Naming

--- frontend/src/KalenderPage.jsx
+++ frontend/src/KalenderPage.jsx
@@ -81,8 +81,8 @@ function KalenderPage() {
       {/* NEUER TV HEADER */}
       <div className="tv-header">
         <div className="header-logo-container">
-          <h2 className="header-logo tv-logo">BTL-Kalender</h2>
-          <span className="header-subtitle tv-subtitle">J.O Design ™ • TV-Dashboard</span>
+          <h2 className="header-logo tv-logo">Stürtz Heizung und Sanitär GmbH Kalender</h2>
+          <span className="header-subtitle tv-subtitle">BTL-Digital Design™ • TV-Dashboard</span>
         </div>
         <div className="tv-clock">
           {currentTime.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' })} Uhr
